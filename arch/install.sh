e1c7ca2cdc8b6e934ec5aa8f790759a759d504a2
Anant Girdhar <anant.girdhar@proton.me>
2022-10-29 14:06:38 -0400

    Use virtualenv when installing python packages

--- arch/install.sh
+++ arch/install.sh
@@ -3,8 +3,8 @@
 # Install all the things needed to make this a functional system
 
 ./install/install_system.sh
-./install/install_programs.sh
+./install/install_utilities.sh
 ./install/install_AUR.sh
 ./install/install_graphics.sh
 ./install/install_suckless.sh
-./install/install_coding.sh
+./install/install_programming.sh
---
+++ arch/install/install_programming.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Python
+sudo pacman -S --noconfirm python
+sudo pacman -S --noconfirm python-pip
+pip install virtualenv
+# Setup the "main" virtualenv
+[ ! -d "$HOME/.local/share/virtualenv" ] && mkdir "$HOME/.local/share/virtualenv"
+virtualenv $HOME/.local/share/virtualenv/py
+# Install all the packages inside this virtualenv
+source $HOME/.local/share/virtualenv/py/bin/activate
+pip install -r python_requirements_main.txt
+
+# I don't remember why this was needed so I'm commenting it out for now
+# Hopefully nothing breaks, but if something does, then we'll know!
+#sudo pacman -S --noconfirm python-pyqt5 qt5-svg
+
+# Misc programming tools
+sudo pacman -S --noconfirm cmake
+sudo pacman -S --noconfirm gdb
+sudo pacman -S --noconfirm ctags
+sudo pacman -S --noconfirm texlive-most
+
+# Virtualbox
+sudo pacman -S --noconfirm virtualbox virtualbox-host-dkms
