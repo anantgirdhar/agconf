7d3df912f7bf3d89c29fabaa891e72a132269d93
Anant Girdhar <anant.girdhar@proton.me>
2022-12-07 17:30:01 -0500

    Update multiscreen keybindings

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -210,9 +210,9 @@ globalkeys = gears.table.join(
 
   -- Layout manipulation
   --TODO: What do these do??
-  awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+  awful.key({ modkey, }, "h", function () awful.screen.focus_relative( 1) end,
     {description = "focus the next screen", group = "screen"}),
-  awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+  awful.key({ modkey, }, "l", function () awful.screen.focus_relative(-1) end,
     {description = "focus the previous screen", group = "screen"}),
   awful.key({ modkey, }, "Tab",
     function ()
