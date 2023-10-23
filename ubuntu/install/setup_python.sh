edd02c6d95ec700f4c4395c52ebd91542ee64b5d
Anant Girdhar <anant.girdhar@proton.me>
2023-10-22 21:06:46 -0400

    Update python installation script
    When I tried to run this script on Ubuntu 23.04, I got an
    externally-managed-environment error. So I changed the script to install
    virtualenv through apt (instead of pip) and then use it to create a
    virtual environment in which the rest of python is set up like normal.

--- ubuntu/install/setup_python.sh
+++ ubuntu/install/setup_python.sh
@@ -2,11 +2,11 @@
 
 # Requires python and pip to already be installed
 
-pip install virtualenv
+sudo apt install python3-virtualenv
 # Setup the "main" virtualenv
 [ ! -d "$HOME/.local/share/virtualenv" ] && mkdir -p "$HOME/.local/share/virtualenv"
-$HOME/.local/bin/virtualenv $HOME/.local/share/virtualenv/py
+virtualenv $HOME/.local/share/virtualenv/py
 # Install all the packages inside this virtualenv
-source $HOME/.local/share/virtualenv/py/bin/activate
-pip install -r python_requirements_main.txt
+. $HOME/.local/share/virtualenv/py/bin/activate
+pip install -r ./install/python_requirements_main.txt
 deactivate
