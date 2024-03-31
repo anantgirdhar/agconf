6e5d63f58a726544bbf16a8fe4d7a7717266ba99
Anant Girdhar <anant.girdhar@proton.me>
2024-03-30 20:33:42 -0400

    Update awesome window rules

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -385,7 +385,8 @@ awful.rules.rules = {
         "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
         "Wpa_gui",
         "veromix",
-        "xtightvncviewer"
+        "xtightvncviewer",
+        "matplotlib",
       },
       -- Note that the name property shown in xprop might be set slightly after creation of the client
       -- and the name shown there might not match defined rules here.
@@ -415,7 +416,8 @@ awful.rules.rules = {
       class = {
         "firefox" ,
         "Brave-browser",
-        "librewolf-default"
+        "librewolf-default",
+        "Chromium"
       }
     },
     properties = { screen = 1, tag = "9" }
